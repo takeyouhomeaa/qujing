d3bbad239aeab7f12d05b52d1ae69b46b9a48dcf
root11 <18030389136@163.com>
2020-05-10 22:27:55 +0800

    重构项目初始化

---
+++ src/main/java/edu/fzu/qujing/QujingApplication.java
@@ -0,0 +1,13 @@
+package edu.fzu.qujing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QujingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QujingApplication.class, args);
+    }
+
+}
